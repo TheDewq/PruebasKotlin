7e0806ae5e9d5c3927e884fd5adc2d2c5457390c
Carlos O <orjuelacarlos80@gmail.com>
2024-05-28 20:17:59 -0500

    Mas progreso

--- PruebasCompose/app/src/main/java/com/example/pruebascompose/ui/Login.kt
+++ PruebasCompose/app/src/main/java/com/example/pruebascompose/ui/Login.kt
@@ -1,5 +1,6 @@
 package com.example.pruebascompose.ui
 
+import android.annotation.SuppressLint
 import android.content.res.Configuration
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.animateFloat
@@ -8,19 +9,45 @@ import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ButtonElevation
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.pruebascompose.R
 import com.example.pruebascompose.ui.theme.colors
 
 class Login {
@@ -41,15 +68,67 @@ class Login {
             modifier = Modifier
                 .fillMaxHeight()
                 .fillMaxWidth()
-                .background(Brush.linearGradient(backgroundGradient, start = Offset(offset,offset)))
+                .background(
+                    Brush.linearGradient(
+                        backgroundGradient,
+                        start = Offset(offset, offset)
+                    )
+                )
+                .padding(all = 10.dp)
         ){
-            Text(text = "Carajo")
+            Column {
+                Text(text = "Carajo", fontSize = 40.sp, fontFamily = FontFamily.Serif);
+                ElevatedButton(
+                    onClick = { println("sirve esta cosa") },
+
+                ) {
+                    Text(text = "boton")
+                }
+            }
+
         }
     }
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @Composable
+    fun scafold(){
+        Scaffold (
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            topBar = { TopAppBar(title = { Text(text = "alv")}) },
+            bottomBar = {
+                BottomAppBar {
+                    NavigationBar {
+                        NavigationBarItem(
+                            selected = true,
+                            onClick = {
+                                      println("hola mi vida")
+                                      },
+                            icon = {
+                                Icon(painter = painterResource(id = R.drawable.win95), contentDescription = "", modifier = Modifier.size(10.dp))
+                            },
+                            label = {
+                                Text(text = "seccion 1")
+                            }
+                        )
+                    }
+                }
+            }
+
+        ){
+
+        }
+    }
+
+
+
+
+
 
     @Preview
     @Composable
     fun preview_login(){
-        screen()
+        scafold()
     }
 }
